bb1d13042ed0a2f0eed01987444c7dff420ae607
Aruniya <121487536+kanaame@users.noreply.github.com>
2024-05-13 10:13:31 +0530

    Updated Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -216,7 +216,7 @@ function Home() {
           </section>
          
           <section className="sec6">
-          <p className="copyright">Copyright © 2024 Aruniya /\/ Aruliya Asokan</p>
+          <p className="copyright">Made with ❤ from scratch 2024 by Aruniya & Aruliya Asokan</p>
 
           </section>
 
