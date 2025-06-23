abeda1bcc8735fb812ca1619f736bf769927e827
AruniyaAsokan <aruniyaasokan@gmail.com>
2025-06-24 00:24:49 +0530

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/final_aruniya_portfolio/'
+  // base: '/final_aruniya_portfolio/'
 })
